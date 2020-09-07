4371185dd7cee8b3b0860d13bd4923234b5021ca
Tony MacDonell <tony@teknision.com>
2020-09-07 15:41:44 -0400

    Re-structured project. Added build scripts. Added Unit Tests

--- src/static/background.js
+++ src/static/background.js
@@ -2,4 +2,4 @@
 Capture the button click on our extension in the toolbar 
 and execute the script in the context of the page:
 */
-chrome.browserAction.onClicked.addListener(() => chrome.tabs.executeScript({file: "extractemail.js"}));
+chrome.browserAction.onClicked.addListener(() => chrome.tabs.executeScript({file: "contentscript.js"}));
